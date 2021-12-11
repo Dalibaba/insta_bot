33da5c0224819491642a41434ebfe31940bb5e0d
Dalibaba <linkedavi@googlemail.com>
2021-12-11 19:25:44 +0100

    like images of a profile

--- pages.py
+++ pages.py
@@ -1,6 +1,5 @@
 from time import sleep
-
-from selenium.webdriver import Keys, ActionChains
+from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 
 
@@ -19,8 +18,8 @@ class LoginPage:
         password_input = self.browser.find_element(By.CSS_SELECTOR, "input[name='password']")
         username_input.send_keys(username)
         password_input.send_keys(password)
+        sleep(2)
         password_input.submit()
-        sleep(3)
         # return page object for the new page
         return self.browser
 
@@ -31,21 +30,14 @@ class StartPage:
 
     def search_profile(self, profile_name):
         # click away pop up questions about login and notifications
-        sleep(1)
+        sleep(3)
         button_not_now = self.browser.find_elements_by_xpath("//*[contains(text(), 'Not Now')]")
         button_not_now[0].click()
         sleep(1)
         button_not_now = self.browser.find_elements_by_xpath("//*[contains(text(), 'Not Now')]")
         button_not_now[0].click()
         sleep(1)
-
-        # search_input = self.browser.find_element(By.CSS_SELECTOR, "input[placeholder='Search']")
-        # search_input.send_keys(profile_name)
-        # sleep(1)
-        # #search_input.submit()
-        # #search_input.click()
-        # search_input.send_keys(Keys.RETURN);
-        # search_input.send_keys(Keys.RETURN);
+        # go to profile url
         url = 'https://www.instagram.com/' + profile_name
         self.browser.get(url)
 
@@ -58,22 +50,29 @@ class ProfilePage:
         self.browser = browser
 
     def get_posts(self, profile_name):
+        soup = BeautifulSoup(self.browser.page_source, "html.parser")
+        all_post_urls = []
+        for a_tag in soup.findAll('a', href=True):
+            href = a_tag["href"]
+            char_count = href.count('/')
+            if char_count == 3:
+                if href[1] == "p":
+                    all_post_urls.append(href)
+
+        print("anzahl posts:", len(all_post_urls))
+        for post_url in all_post_urls:
+            url = 'https://www.instagram.com' + post_url
+            self.like_image(url)
+
+
         # click away pop up questions about login and notifications
-        sleep(1)
-        button_not_now = self.browser.find_elements_by_xpath("//*[contains(text(), 'Not Now')]")
-        button_not_now[0].click()
-        sleep(1)
-        button_not_now = self.browser.find_elements_by_xpath("//*[contains(text(), 'Not Now')]")
-        button_not_now[0].click()
-        sleep(1)
-
-        search_input = self.browser.find_element(By.CSS_SELECTOR, "input[placeholder='Search']")
-        search_input.send_keys(profile_name)
-        sleep(1)
-        #search_input.submit()
-        #search_input.click()
-        search_input.send_keys(Keys.RETURN);
-        search_input.send_keys(Keys.RETURN);
-
-        sleep(2)
         return self.browser
+
+    def like_image(self, url):
+        self.browser.get(url)
+        sleep(2)
+        xpath = "//*[@class='fr66n']"
+        like_button = self.browser.find_element(By.XPATH, xpath)
+        print(like_button)
+        like_button.click()
+        sleep(2)
